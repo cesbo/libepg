fa92d1a6ef214dde3a55bf5bbeef5ebf6c7cebaf
Andrey Dyldin <and@cesbo.com>
2018-09-14 01:28:53 +0500

    tests cleaning

--- tests/xmltv.rs
+++ tests/xmltv.rs
@@ -1,5 +1,6 @@
 extern crate epg;
 use epg::*;
+use std::str;
 
 #[test]
 fn test_parse_programme() {
@@ -33,4 +34,20 @@ fn test_parse_xmltv() {
     // check event
     let p2 = events_iter.next().unwrap();
     assert_eq!(p2.title.get("en").unwrap(), "Title #2");
+}
+
+#[test]
+fn test_assemble_xmltv() {
+    let content: &[u8] = include_bytes!("docs/e2.xml");
+
+    // convert xmltv into epg
+    let mut epg = Epg::default();
+    epg.parse_xml(content).unwrap();
+
+    // convert epg into xmltv
+    let mut target: Vec<u8> = Vec::new();
+    epg.assemble_xml(&mut target).unwrap();
+
+    let xml = str::from_utf8(&target).unwrap();
+    println!("{}", xml);
 }
