9690cbd5ec3dcc24617aa1109058589da677bbf9
Andrey Dyldin <and@cesbo.com>
2019-01-17 02:08:37 +0500

    truncate long description

--- src/epg.rs
+++ src/epg.rs
@@ -101,8 +101,9 @@ impl<'a> From<&'a EpgEvent> for EitItem {
         }
 
         for (lang, desc) in &event.desc {
-            // 5 - additional space for for text stuff
-            let mut text_list = StringDVB::from_str(desc, event.codepage).split(0xFF - Desc4E::min_size());
+            let mut text_list = StringDVB::from_str(desc, event.codepage);
+            text_list.truncate(1000);
+            let mut text_list = text_list.split(0xFF - Desc4E::min_size());
             let mut number: u8 = 0;
             let last_number: u8 = text_list.len() as u8 - 1;
 
