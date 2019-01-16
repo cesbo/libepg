929d38e914291ee00cd3161b6d03a583bb74fc74
Andrey Dyldin <and@cesbo.com>
2019-01-16 23:57:58 +0500

    fix extended event descriptor (need more space for long text)

--- src/epg.rs
+++ src/epg.rs
@@ -101,7 +101,8 @@ impl<'a> From<&'a EpgEvent> for EitItem {
         }
 
         for (lang, desc) in &event.desc {
-            let mut text_list = StringDVB::from_str(desc, event.codepage).split(0xFF - Desc4E::min_size());
+            // 5 - additional space for for text stuff
+            let mut text_list = StringDVB::from_str(desc, event.codepage).split(0xFF - Desc4E::min_size() - 5);
             let mut number: u8 = 0;
             let last_number: u8 = text_list.len() as u8 - 1;
 
